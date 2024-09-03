f764b55db37002c6fdbbddc900d4932eb98b9a6a
John Lorenz <dreamsend08@gmail.com>
2024-09-03 17:33:20 +0800

    Add WebSocket context provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,30 @@ import Home from './components/Home';
 import Convo from './components/Convo';
 import Profile from './components/Profile';
 import ProtectedRoute from './components/ProtectedRoute';
-import { AuthProvider } from './components/AuthProvider';
+import { AuthProvider } from './contexts/AuthProvider';
+import { WebSocketProvider } from './contexts/WebSocketContext';
 
 function App() {
   return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/conversation/:conversationId" element={<ProtectedRoute><Convo /></ProtectedRoute>} />
-          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-        </Routes>
-      </Router>
+      <WebSocketProvider>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/conversation/:conversationId" element={<ProtectedRoute><Convo /></ProtectedRoute>} />
+            <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+          </Routes>
+        </Router>
+      </WebSocketProvider>
     </AuthProvider>
   );
 }
