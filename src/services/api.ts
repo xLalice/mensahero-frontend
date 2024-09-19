31ede4ecb91155c4e49d0af2b9d256c50f255769
John Lorenz <dreamsend08@gmail.com>
2024-09-19 19:58:24 +0800

    fix: api serivice missing /api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true
 });
 
