fee5e18dc2548ff19940b9455b1286d53b4823a1
John Lorenz <dreamsend08@gmail.com>
2024-09-19 19:54:49 +0800

    fix: api service base url to env variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true
 });
 
